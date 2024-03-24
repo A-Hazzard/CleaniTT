9035f26bf158af7940fa7aba9391bfd7d83fdc38
Kalem Mentore <kalem@Kalems-MacBook-Air.local>
2024-03-24 12:29:10 -0400

    leaderboard

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,40 +1,39 @@
 // Express routes for the application
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const Report = require('../models/report');
-const cloudinary = require('cloudinary').v2;
-const geolib = require('geolib');
-const exifParser = require('exif-parser');
-const ExifReader = require('exifreader');
-const User = require('../models/user');
-const Activity = require('../models/activity');
-require('dotenv').config();
+const multer = require("multer");
+const Report = require("../models/report");
+const cloudinary = require("cloudinary").v2;
+const geolib = require("geolib");
+const exifParser = require("exif-parser");
+const ExifReader = require("exifreader");
+const User = require("../models/user");
+const Activity = require("../models/activity");
+require("dotenv").config();
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUDNAME , 
-  api_key: process.env.CLOUDINARY_APIKEY , 
-  api_secret: process.env.CLOUDINARY_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+  api_key: process.env.CLOUDINARY_APIKEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
-
 
 const upload = multer({ storage: multer.memoryStorage() }); // Multer configuration with memory storage
 // Define difficulty levels and points per material
 const difficultyLevels = {
-  'metal': 10,     // Adjust points based on difficulty
-  'plastic': 5,
+  metal: 10, // Adjust points based on difficulty
+  plastic: 5,
   // Add more materials and their corresponding points as needed
 };
-router.post('/reports', upload.single('photo'), async (req, res) => {
+router.post("/reports", upload.single("photo"), async (req, res) => {
   try {
     if (!req.file) {
-      throw new Error('No file uploaded');
+      throw new Error("No file uploaded");
     }
 
     let { description, latitude, longitude, materials } = req.body;
 
-    if (!materials || typeof materials !== 'object') {
-      throw new Error('Invalid materials data');
+    if (!materials || typeof materials !== "object") {
+      throw new Error("Invalid materials data");
     }
 
     let photoBuffer = req.file.buffer; // Access the file buffer directly
@@ -48,12 +47,14 @@ router.post('/reports', upload.single('photo'), async (req, res) => {
     }
 
     // Update user's points
-    await User.findByIdAndUpdate(req.user._id, { $inc: { points: totalPoints } });
+    await User.findByIdAndUpdate(req.user._id, {
+      $inc: { points: totalPoints },
+    });
 
     // Save activity
     await Activity.create({
       userId: req.user._id,
-      type: 'report',
+      type: "report",
       points: totalPoints,
       // Additional fields as needed
     });
@@ -64,38 +65,41 @@ router.post('/reports', upload.single('photo'), async (req, res) => {
 
     // Upload image buffer to Cloudinary using upload_stream
     const uploadResponse = await new Promise((resolve, reject) => {
-      const uploadStream = cloudinary.uploader.upload_stream({ folder: "reports" }, (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      });
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: "reports" },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      );
       uploadStream.end(photoBuffer);
     });
 
     // Save Cloudinary URL in the database
-    const report = new Report({ 
-      description, 
+    const report = new Report({
+      description,
       photo: uploadResponse.secure_url, // Use the secure URL returned by Cloudinary
-      latitude, 
-      longitude 
+      latitude,
+      longitude,
     });
     await report.save();
 
-    res.status(201).send('Report saved');
+    res.status(201).send("Report saved");
   } catch (error) {
-    console.error('Error saving report:', error.message);
-    res.status(400).send('Error saving report: ' + error.message);
+    console.error("Error saving report:", error.message);
+    res.status(400).send("Error saving report: " + error.message);
   }
 });
 
 // Route to get report data
-router.get('/reports', async (req, res) => {
+router.get("/reports", async (req, res) => {
   try {
     const reports = await Report.find({});
     const clusters = calculateClusters(reports, 50);
     res.json({ reports, clusters });
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 });
 
@@ -103,51 +107,58 @@ function calculateClusters(reports, radius) {
   const clusters = [];
   const processedReportIds = new Set();
 
-  reports.forEach(report => {
+  reports.forEach((report) => {
     // Skip if this report has already been processed as part of a cluster
     if (processedReportIds.has(report._id.toString())) {
       return;
     }
 
-    const nearbyReports = reports.filter(otherReport => {
+    const nearbyReports = reports.filter((otherReport) => {
       // Do not compare the report with itself and skip already processed reports
-      return report._id.toString() !== otherReport._id.toString() &&
-             !processedReportIds.has(otherReport._id.toString()) &&
-             geolib.isPointWithinRadius(
-               { latitude: report.latitude, longitude: report.longitude },
-               { latitude: otherReport.latitude, longitude: otherReport.longitude },
-               radius
-             );
+      return (
+        report._id.toString() !== otherReport._id.toString() &&
+        !processedReportIds.has(otherReport._id.toString()) &&
+        geolib.isPointWithinRadius(
+          { latitude: report.latitude, longitude: report.longitude },
+          { latitude: otherReport.latitude, longitude: otherReport.longitude },
+          radius
+        )
+      );
     });
 
     // If at least 2 other reports are in the same area (3 reports make a cluster)
     if (nearbyReports.length >= 2) {
       // Mark all reports in this cluster as processed
-      nearbyReports.forEach(nr => processedReportIds.add(nr._id.toString()));
+      nearbyReports.forEach((nr) => processedReportIds.add(nr._id.toString()));
       processedReportIds.add(report._id.toString());
-      
+
       // Add the original report to the list
       nearbyReports.push(report);
 
       // Calculate the center point of the cluster
-      const averageCoords = geolib.getCenter(nearbyReports.map(r => ({ latitude: r.latitude, longitude: r.longitude })));
+      const averageCoords = geolib.getCenter(
+        nearbyReports.map((r) => ({
+          latitude: r.latitude,
+          longitude: r.longitude,
+        }))
+      );
 
       // Add cluster to list
       clusters.push({
         center: averageCoords,
-        count: nearbyReports.length
+        count: nearbyReports.length,
       });
-    }else console.log('no nearby reports')
+    } else console.log("no nearby reports");
   });
 
   return clusters;
 }
 
-router.post('/lazyReport', upload.single('photo'), async (req, res) => {
+router.post("/lazyReport", upload.single("photo"), async (req, res) => {
   try {
     if (!req.file) {
-      consol.error('No file uploaded.');
-      return res.status(400).send('No file uploaded.');
+      consol.error("No file uploaded.");
+      return res.status(400).send("No file uploaded.");
     }
 
     const { description } = req.body;
@@ -162,8 +173,8 @@ router.post('/lazyReport', upload.single('photo'), async (req, res) => {
         latitude = result.tags.GPSLatitude;
         longitude = result.tags.GPSLongitude;
       } else {
-        console.error('EXIF data is missing GPS information.')
-        throw new Error('EXIF data is missing GPS information.');
+        console.error("EXIF data is missing GPS information.");
+        throw new Error("EXIF data is missing GPS information.");
       }
     } catch (exifError) {
       // Try with exifreader if exif-parser fails
@@ -174,44 +185,48 @@ router.post('/lazyReport', upload.single('photo'), async (req, res) => {
           latitude = tags.GPSLatitude.description;
           longitude = tags.GPSLongitude.description;
         } else {
-          console.error('Location data not found in image EXIF.')
-          return res.status(400).send('Location data not found in image EXIF.');
+          console.error("Location data not found in image EXIF.");
+          return res.status(400).send("Location data not found in image EXIF.");
         }
       } catch (exifReaderError) {
-        console.error('Could not extract EXIF data with any library.')
-        return res.status(400).send('Could not extract EXIF data with any library.');
+        console.error("Could not extract EXIF data with any library.");
+        return res
+          .status(400)
+          .send("Could not extract EXIF data with any library.");
       }
     }
 
     // Proceed with Cloudinary upload
     const uploadResponse = await new Promise((resolve, reject) => {
-      const uploadStream = cloudinary.uploader.upload_stream({ folder: "reports" }, (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
-      });
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: "reports" },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      );
       uploadStream.end(photoBuffer);
     });
 
     // Save Cloudinary URL in the database
-    const report = new Report({ 
-      description, 
+    const report = new Report({
+      description,
       photo: uploadResponse.secure_url,
-      latitude, 
-      longitude 
+      latitude,
+      longitude,
     });
 
     await report.save();
 
-    res.status(201).json({ message: 'Lazy report saved', report: report });
-
+    res.status(201).json({ message: "Lazy report saved", report: report });
   } catch (error) {
-    console.error('Error saving lazy report:', error.message);
-    res.status(500).send('Server error while saving lazy report.');
+    console.error("Error saving lazy report:", error.message);
+    res.status(500).send("Server error while saving lazy report.");
   }
 });
 
 // Route to get user profile
-router.get('/profile', async (req, res) => {
+router.get("/profile", async (req, res) => {
   try {
     const { userId } = req.query;
     const user = await User.findById(userId);
@@ -221,17 +236,19 @@ router.get('/profile', async (req, res) => {
     }
     res.status(200).json(user);
   } catch (error) {
-    console.error('Error fetching user profile:', error.message);
-    res.status(500).send('Server error');
+    console.error("Error fetching user profile:", error.message);
+    res.status(500).send("Server error");
   }
 });
 
 // Update User Profile
-router.put('/user/:id', async (req, res) => {
+router.put("/user/:id", async (req, res) => {
   try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     if (!updatedUser) {
-      return res.status(404).send('User not found');
+      return res.status(404).send("User not found");
     }
     res.json(updatedUser);
   } catch (error) {
@@ -239,17 +256,16 @@ router.put('/user/:id', async (req, res) => {
   }
 });
 
-
 // Register route
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
     // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      console.error('User already exists')
-      return res.status(400).send('User already exists');
+      console.error("User already exists");
+      return res.status(400).send("User already exists");
     }
 
     // Create new user
@@ -260,32 +276,52 @@ router.post('/register', async (req, res) => {
 
     res.status(201).json(newUser);
   } catch (error) {
-    console.error('Error registering user:', error.message);
-    res.status(500).send('Server error');
+    console.error("Error registering user:", error.message);
+    res.status(500).send("Server error");
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     // Check if user exists
     const user = await User.findOne({ email });
     if (!user) {
-      console.error('Invalid email')
-      return res.status(400).send('Invalid credentials');
+      console.error("Invalid email");
+      return res.status(400).send("Invalid credentials");
     }
 
     // Compare passwords
     if (user.password !== password) {
-      console.error('Invalid password')
-      return res.status(400).send('Invalid credentials');
+      console.error("Invalid password");
+      return res.status(400).send("Invalid credentials");
     }
-    console.log('logged in')
+    console.log("logged in");
     // Redirect to frontend/index.html upon successful login
-    res.status(200).json(user)
+    res.status(200).json(user);
   } catch (error) {
-    console.error('Error logging in user:', error.message);
-    res.status(500).send('Server error');
+    console.error("Error logging in user:", error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    for (const user of users) {
+      const activityList = await Activity.find({ userId: user._id });
+
+      let totalPoints = 0;
+      for (const activity of activityList) {
+        totalPoints += activity.points;
+      }
+      user.points = totalPoints;
+      await user.save();
+    }
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send("Server error");
   }
 });
 
